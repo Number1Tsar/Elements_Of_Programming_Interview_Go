872643504a0f7304f0d0a76fa19a7db53d523800
Sulav Adhikari <sulavadhikari@Shirin.local>
2022-06-25 18:20:16 -0700

    linked list

---
+++ linkedList/merge_two_sorted_list.go
@@ -0,0 +1,32 @@
+package linkedList
+
+/*
+	Leetcode Problem 21.
+	Merge Two Sorted List
+*/
+
+type ListNode struct {
+	Val  int
+	Next *ListNode
+}
+
+func mergeTwoLists(list1 *ListNode, list2 *ListNode) *ListNode {
+	dummyHead := new(ListNode)
+	current := dummyHead
+	for list1 != nil && list2 != nil {
+		if list1.Val < list2.Val {
+			current.Next = list1
+			list1 = list1.Next
+		} else {
+			current.Next = list2
+			list2 = list2.Next
+		}
+		current = current.Next
+	}
+	if list1 != nil {
+		current.Next = list1
+	} else {
+		current.Next = list2
+	}
+	return dummyHead.Next
+}
---
+++ linkedList/reverse_a_single_sublist.go
@@ -0,0 +1,10 @@
+package linkedList
+
+/*
+	Leetcode Question 92
+	Reverse Linked List II
+*/
+
+func reverseBetween(head *ListNode, left int, right int) *ListNode {
+	return nil
+}
