64bbee5a57e13fa265c79e819ab9b18ce8ef3c2d
Sulav Adhikari <sulavadhikari@Shirin.local>
2022-06-27 14:39:48 -0700

    question 92

--- linkedList/reverse_a_single_sublist.go
+++ linkedList/reverse_a_single_sublist.go
@@ -6,5 +6,29 @@ package linkedList
 */
 
 func reverseBetween(head *ListNode, left int, right int) *ListNode {
-	return nil
+	dummyHead := new(ListNode)
+	dummyHead.Next = head
+	index := 0
+	start := dummyHead
+	for start != nil {
+		if left == index+1 {
+			break
+		}
+		start = start.Next
+		index += 1
+	}
+	var p, q, r *ListNode
+	p = nil
+	q = start.Next
+	index += 1
+	for q != nil && index <= right {
+		r = q.Next
+		q.Next = p
+		p = q
+		q = r
+		index += 1
+	}
+	start.Next.Next = q
+	start.Next = p
+	return dummyHead.Next
 }
